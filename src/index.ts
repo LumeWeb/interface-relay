eb2d441670536c8a865e1a49a8ed11b1823ea072
Derrick Hammer <derrick@derrickhammer.com>
2022-08-28 22:01:51 -0400

    *Initial version extracted from @lumeweb/relay

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import type { JSONSchemaType } from "ajv";
+export interface RPCRequest {
+  bypassCache?: boolean;
+  module: string;
+  method: string;
+  data: any;
+}
+
+export interface RPCResponse {
+  updated?: number;
+  data?: any;
+  error?: string;
+}
+
+export interface RPCMethod {
+  cacheable: boolean;
+  handler: (
+    request: RPCRequest,
+    sendStream: (stream: AsyncIterable<Uint8Array>) => void
+  ) => Promise<RPCResponse | null>;
+}
+
+// @ts-ignore
+export const RPC_REQUEST_SCHEMA: JSONSchemaType<RPCRequest> = {
+  type: "object",
+  properties: {
+    module: {
+      type: "string",
+    },
+    method: {
+      type: "string",
+    },
+    data: {
+      type: ["number", "string", "boolean", "object", "array"],
+    },
+    bypassCache: {
+      type: "boolean",
+      nullable: true,
+    },
+  },
+};
+
+export interface StreamFileResponse {
+  data?: Uint8Array;
+  done: boolean;
+}
+
+export interface PluginAPI {
+  config: any;
+  registerMethod: (methodName: string, method: RPCMethod) => void;
+  loadPlugin: (moduleName: string) => Promise<Plugin>;
+  getMethods: () => string[];
+}
+
+export type PluginFunction = (api: PluginAPI) => Promise<void>;
+
+export interface Plugin {
+  name: string;
+  plugin: PluginFunction;
+  exports?: any;
+  default?: Plugin;
+}
+
+export type RPCStreamHandler = (
+  stream: AsyncIterable<Uint8Array>
+) => Promise<RPCResponse>;
