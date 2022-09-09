17fa55f15657d093db9d9a993132d62fb54d00d8
Derrick Hammer <derrick@derrickhammer.com>
2022-09-09 03:20:52 -0400

    *Update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,8 @@
+/// <reference types="node" />
+import tls from "tls";
+import type { Logger } from "loglevel";
+import type { Ed25519Keypair, Err } from "libskynet";
+import type express from "express";
 export interface RPCRequest {
   bypassCache?: boolean;
   module: string;
@@ -25,6 +30,20 @@ export interface PluginAPI {
   registerMethod: (methodName: string, method: RPCMethod) => void;
   loadPlugin: (moduleName: string) => Promise<Plugin>;
   getMethods: () => string[];
+  ssl: {
+    setContext: (context: tls.SecureContext) => void;
+    getContext: () => tls.SecureContext;
+    getSaved: (retry: boolean) => Promise<boolean | SslData>;
+    set: (cert: IndependentFileSmall, key: IndependentFileSmall) => void;
+    get: () => SslData;
+    save: () => Promise<void>;
+  };
+  appRouter: {
+    get: () => express.Router;
+    set: (newRouter: express.Router) => void;
+    reset: () => void;
+  };
+  logger: Logger;
 }
 export declare type PluginFunction = (api: PluginAPI) => Promise<void>;
 export interface Plugin {
@@ -36,4 +55,26 @@ export interface Plugin {
 export declare type RPCStreamHandler = (
   stream: AsyncIterable<Uint8Array>
 ) => Promise<RPCResponse>;
+export declare type OverwriteDataFn = (newData: Uint8Array) => Promise<Err>;
+export declare type ReadDataFn = () => Promise<[Uint8Array, Err]>;
+export interface IndependentFileSmallMetadata {
+  largestHistoricSize: bigint;
+}
+export interface IndependentFileSmall {
+  dataKey: Uint8Array;
+  fileData: Uint8Array;
+  inode: string;
+  keypair: Ed25519Keypair;
+  metadata: IndependentFileSmallMetadata;
+  revision: bigint;
+  seed: Uint8Array;
+  skylink: string;
+  viewKey: string;
+  overwriteData: OverwriteDataFn;
+  readData: ReadDataFn;
+}
+export interface SslData {
+  cert?: IndependentFileSmall;
+  key?: IndependentFileSmall;
+}
 //# sourceMappingURL=index.d.ts.map
