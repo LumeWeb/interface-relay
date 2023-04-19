3231c18e087874ca10b0c9ea4b4a023bd52e93e3
Derrick Hammer <derrick@derrickhammer.com>
2023-04-18 22:54:35 -0400

    *Update SSL types

--- src/ssl.ts
+++ src/ssl.ts
@@ -1,18 +1,25 @@
 import type tls from "tls";
 
 export type SSLManagerRenewHandler = (domain: string) => Promise<boolean>;
+export type SSLCert = string | Buffer | Array<string | Buffer>;
+
 declare class SSLManager {
-  private _context?;
-  private _key?;
-  private _cert?;
-  private _domain;
-  private _renewHandler?;
   constructor(domain: string);
+
   get context(): tls.SecureContext;
+
   set privateKey(key: Buffer);
-  set cert(cert: Buffer);
-  private _maybeUpdateContext;
+
+  set cert(cert: SSLCert);
+
   renew(): Promise<boolean>;
-  get enabled(): any;
+
+  get enabled(): boolean;
+
+  get ready(): boolean;
+
+  get renewHandler(): SSLManagerRenewHandler;
+  set renewHandler(value: SSLManagerRenewHandler);
 }
+
 export default SSLManager;
