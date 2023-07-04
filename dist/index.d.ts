0030db4237a2193f8842a05687c79dd39415ba1e
Derrick Hammer <derrick@derrickhammer.com>
2023-07-04 03:59:00 -0400

    refactor: move to ci and update deps

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./rpc.js";
-export * from "./plugin.js";
-export * from "./swarm.js";
-//# sourceMappingURL=index.d.ts.map
--- dist/swarm.d.ts
+++
@@ -1,6 +0,0 @@
-export type ProtocolHandler = (peer: any, muxer: any) => void;
-export declare class ProtocolManager {
-  constructor(swarm: any);
-  register(name: string, handler: ProtocolHandler): boolean;
-}
-//# sourceMappingURL=swarm.d.ts.map
